4cb4f7753998dcef02d6c36b1c00888da8f50f58
Tatyana Tsvetkova <tsvetta@gmail.com>
2019-05-28 10:22:44 +0300

    Обновлены фотки

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 function init() {
   const subscribeButtonLink = document.getElementById('subscribe-button-link');
   const subscribeInput = document.getElementById('subscribe-input');
-  const subscribeBox = document.getElementById('subscribe-box');
+  const subscribeForm = document.getElementById('subscribe-form');
+  const logoBox = document.getElementById('logo-box');
 
   subscribeButtonLink.addEventListener('click', () => {
     window.scrollTo({
-      top: subscribeBox.offsetTop - 300,
+      top: subscribeForm.offsetTop - 300,
       behavior: 'smooth',
     });
 
-    subscribeBox.classList.add('training-subscribe_blink');
+    subscribeForm.classList.add('training-subscribe_blink');
 
     setTimeout(() => {
       subscribeInput.focus();
     }, 500);
 
     setTimeout(() => {
-      subscribeBox.classList.remove('training-subscribe_blink');
+      subscribeForm.classList.remove('training-subscribe_blink');
     }, 1500);
+  });
+
+  // window.addEventListener('scroll', () => {
+  //   console.log(window.pageYOffset);
+
+  //   if (window.pageYOffset > 170) {
+  //     logoBox.classList.add('_active');
+
+  //     return;
+  //   }
+
+  //   logoBox.classList.remove('_active');
+  // });
+
+  subscribeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(subscribeInput.value);
   });
 }
 
